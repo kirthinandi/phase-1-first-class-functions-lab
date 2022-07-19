9449541e82a4c05f9dac229fa650e183117e1a40
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-07-18 22:37:34 -0700

    commit tree 84d16b2ca4ee613e5588d50f07288dca0aa25933

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x) {
+    return function (fare) {
+        return fare*x;
+    }
+}
+
+function fareDoubler(fare){
+    const y = createFareMultiplier(2)(fare);
+    return y;
+}
+
+function fareTripler(fare) {
+    const y = createFareMultiplier(3)(fare);
+    return y;
+}
+
+function selectDifferentDrivers(drivers, driverFunction) {
+    if (driverFunction === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers);
+    }
+    else if (driverFunction === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers);
+    }
+}
